3560e72ce8c60d47c0ff8ccd411713cfc7e28188
William Killerud <william@killerud.com>
2022-01-25 16:21:53 +0100

    fix: oppdater breakpoints
    affects: @fremtind/jkl-core
    
    Gjør om på minsteverdien, og bytt om på semantikken bak brekkpunktene.
    Brekkpunktet går fra å være maksimumsverdien til størrelsen, til å bli
    minimumsstørrelsen (breaking into medium, for eksempel).
    
    Fas ut small-variabelen.
    
    Oppdater hjelpefunksjonene til å bruke de nye variablene. Om du har
    brukt disse trenger du ikke foreta deg noe.
    
    Har du brukt variablene direkte kan du fortsette med det, men vit
    at small er lik medium nå, slik at du kanskje kan slette noe kode.
    
    Husk også at du står fritt til å definere dine egne breakpoints,
    hvor enn du trenger det!
    
    ISSUES CLOSED: #2605

--- packages/core/src/utils/breakpoints.ts
+++ packages/core/src/utils/breakpoints.ts
@@ -1,6 +1,7 @@
 export const breakpoints = {
-    small: 768,
-    medium: 992,
+    /** @deprecated `small` og `medium` er identiske. En `small device` går fra 0 til `medium - 1`. */
+    small: 680,
+    medium: 680,
     large: 1200,
     xl: 1600,
 };
